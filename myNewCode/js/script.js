01e0454eadc862f9b91c174f8b3668e06dc27678
EsanMilos <milos.esan@bridgewaterlabs.com>
2020-10-29 17:34:38 +0100

    finale

--- myNewCode/js/script.js
+++
@@ -1,6 +0,0 @@
-let el = document.getElementById('sidebarCollapse');
-let el1 = document.getElementById('sidebar')
-
-el.addEventListener("click", function() {
-    el1.classList.toggle("active");
-});
